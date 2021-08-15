0ac4b2e31da9478b4f12e5019c1b4026ce4c19e4
zg <zgaihl@126.com>
2021-08-15 21:02:19 +0800

    my second commit by idea

--- src/main/java/com/atguigu/git/Git_Test.java
+++ src/main/java/com/atguigu/git/Git_Test.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class Git_Test {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git01!");
     }
 }
