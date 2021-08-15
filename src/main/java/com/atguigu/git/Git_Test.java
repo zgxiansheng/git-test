39e40616e9b24183cf948394dccbfefca58903a2
zg <zgaihl@126.com>
2021-08-15 21:03:20 +0800

    my third commit by idea

--- src/main/java/com/atguigu/git/Git_Test.java
+++ src/main/java/com/atguigu/git/Git_Test.java
@@ -8,5 +8,6 @@ public class Git_Test {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git01!");
+        System.out.println("hello git02!");
     }
 }
