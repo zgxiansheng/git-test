40e801dd3429e30051b8d5a27d62fdadc340dc9d
zg <zgaihl@126.com>
2021-08-15 20:58:07 +0800

    my first commit by idea

---
+++ src/main/java/com/atguigu/git/Git_Test.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zgstart
+ * @create 2021-08-15 20:51
+ */
+public class Git_Test {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
